16a021b4f9fa149f1eb24d606f1191f58fe9e43e
Lisvan <lisvanpp88@gmail.com>
2024-10-20 00:48:09 -0700

    translate from spanish to english

--- database.py
+++ database.py
@@ -13,20 +13,6 @@ Base = declarative_base()
 
 Base.metadata.create_all(bind=engine)
 
-# Crear las tablas en la base de datos
-#Base.metadata.create_all(bind=engine)
-
-#from sqlalchemy import inspect
-
-#inspector = inspect(engine)
-#print(inspector.get_table_names())
-
-
-
-
-#print(engine.table_names())  # Deprecated en SQLAlchemy 1.4+
-
-# Función para obtener una sesión de base de datos
 def get_db():
     db = SessionLocal()
     try:
